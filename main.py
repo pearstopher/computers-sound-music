25cf0f417558d8706547af81a4b12795004d1185
Pearstopher <pearstopher@gmail.com>
2022-03-10 10:59:17 -0800

    imports and initial instructions

--- main.py
+++ main.py
@@ -3,6 +3,22 @@
 # Christopher Juncker
 #
 
+import wave
+# INSTALLING/IMPORTING sounddevice:
+#   (sounddevice was suggested as a simpler alternative to pyaudio)
+#   >pip install sounddevice
+#   import sounddevice
+#
+# INSTALLING/IMPORTING pyaudio:
+#   suggestion to install visual c++ before installing pyaudio
+#   https://docs.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist
+#
+#   the above didn't work so I tried installing a 'wheel' for my version of python here:
+#   https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio
+#   then I `pip install The_file_I_downloaded.whl`
+#   (source: https://stackoverflow.com/questions/59467023)
+import pyaudio
+
 
 # The Assignment
 #
@@ -36,6 +52,7 @@
 #       Do not shell out to an external program for this: use an audio library.
 #
 #       Each time your program is run, it should do all three of these things.
+#
 
 
 def main():
