805be021f231fb39bb6ec9ded4ed5821a73dd97e
Pearstopher <pearstopher@gmail.com>
2022-04-08 12:24:21 -0700

    removed unused sections and updated comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # CS410 Computers, Sound & Music
 #
-# Playing with Fourier transform
+# Plays a short version of the song "Frequency"
 #
 # Christopher Juncker
 
@@ -53,38 +53,13 @@ class Samples:
         wf.write(file, self.sample_rate, samples)
 
 
-# utility function: print a spectrogram from the STFT
-def spectrogram(f, t, zxx):
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.istft.html
-    plt.figure()
-    plt.pcolormesh(t, f, np.abs(zxx), vmin=0, vmax=8192, shading='gouraud')  # 8192 = max sin amplitude
-    # plt.ylim([f[1], f[-1]])  # this shows the whole range of possible frequencies
-    plt.ylim(400, 1000)  # this narrows down on the frequencies I am making (440 and optionally 880 hz)
-    plt.title('STFT Magnitude')
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.yscale('log')
-    plt.show()
-
-
-# utility function: print a graph of the sine waves before and after
-def graph_sines(old_samples, new_samples):
-    periods = 4  # how many periods of the original sine wave to display
-    plt.plot(old_samples[0:int(48000 / 440 * periods)])
-    plt.plot(new_samples[0:int(48000 / 440 * periods)])
-    plt.xlabel("1/48000 s")
-    plt.ylabel("amplitude")
-    plt.show()
-
-
 def main():
-    # play unmodified audio tracks
+    # play the song
     s = Samples()
     s.play()
 
     # apply Short Time Fourier Transform (STFT) to samples
-    fs = 48000
-    f, t, zxx = signal.stft(s.master, fs=fs, nperseg=fs/2)
+    f, t, zxx = signal.stft(s.master, fs=s.sample_rate, nperseg=s.sample_rate/2)
 
     # shift all the frequencies up(+) or down(-) by a random amount
     shift = -30
@@ -97,19 +72,11 @@ def main():
 
     # apply Inverse Short Time Fourier Transform (ISTFT) to re-create samples
     old_samples = np.copy(s.master)
-    _, new_samples = signal.istft(zxx, fs)
+    _, new_samples = signal.istft(zxx, s.sample_rate)
     s.master = np.copy(new_samples)
 
     # play the reconstructed sin wave
     s.play()
-
-    # display the spectrogram of the original frequencies
-    # spectrogram(f, t, zxx)
-
-    # display the first few periods of the sin wave alongside its reconstruction
-    # graph_sines(old_samples, new_samples)
-    # you can see that the new wave (yellow) is oscillating slower than the original (blue)
-    # this is because its frequency is now slightly lower
 
 
 if __name__ == '__main__':
