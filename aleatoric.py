7a9ca8763ffca73283541aa9629d684e6b8f3cd5
Pearstopher <pearstopher@gmail.com>
2022-05-19 10:20:21 -0700

    added main audio loop

--- aleatoric.py
+++ aleatoric.py
@@ -4,11 +4,10 @@
 #
 
 import numpy as np
-import scipy.io.wavfile as wf
 import pyaudio
 import math
 import argparse
-import random
+
 
 # THE ASSIGNMENT
 # You will build a generator that plays a sequence of notes, most of which are randomly
@@ -165,17 +164,21 @@ args = parser.parse_args()
 # Each measure will start with the root note of the major scale, and then continue
 # with randomly-selected notes of the major scale for the remaining beats. These
 # randomly-selected tones should be chosen from scale notes 2..8.
-def random_notes():
-    # create an array of random midi notes
-    notes = np.random.randint(1, 9, size=args.sig)
+def generate_notes():
+    # create an array of the root's major scale
+    major_scale = np.array([2, 4, 5, 7, 9, 11, 12])
+    major_scale += args.root
+
+    # create an array of random notes from the major scale
+    note_array = np.array([np.random.choice(major_scale) for _ in range(args.beats)])
 
     # set the first element to the root
-    notes[0] = args.root
+    note_array[0] = args.root
 
     # convert the midi notes to frequencies
-    notes = midi_to_freq(notes)
+    note_array = midi_to_freq(note_array)
 
-    return notes
+    return note_array
 
 
 ###############
@@ -184,7 +187,7 @@ def random_notes():
 
 # The formula you want for a given amplitude (0..1) given a volume knob setting  (0..10)
 def vol_to_amp(volume):
-    amplitude = 10 * ((-6*(10 - volume)) / 20)
+    amplitude = 10 ** ((-6*(10 - volume)) / 20)
     return amplitude
 
 
@@ -209,11 +212,11 @@ def envelope(samples):
     samples[0:envelope_size] *= ramp_up
 
     # instead of creating a ramp down, just flip the array and use the same ramp again :D
-    samples = samples.flip
+    samples = np.flip(samples)
     samples[0:envelope_size] *= ramp_up
 
     # flip back and return the samples with the envelope applied
-    samples = samples.flip
+    samples = np.flip(samples)
     return samples
 
 
@@ -222,212 +225,36 @@ def envelope(samples):
 ###############
 
 # Based on the Pseudocode the assignment instructions
-#
-# repeat until interrupted
-#     play root frequency for beat interval (square wave, accent volume, ramp)
-#     for remaining beats
-#         k ← random key from scale up 1 to 8 scale steps from root
-#         play frequency k for beat interval (sine wave, volume, ramp)
+
+# first, set up pyaudio stream
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=48000,
+                output=True)
+
+# "repeat until interrupted
+while True:
+    # generate the notes for this measure
+    notes = generate_notes()
+    print(notes)
+
+    # "play root frequency for beat interval (square wave, accent volume, ramp)
+    bps = args.bpm/60  # beats per second
+    spb = samples_per_beat = int(48000 / bps)  # samples per beat
+    note = np.asarray([square_wave(notes[0], t) for t in np.linspace(0, (1/bps), spb)], dtype=np.float32)
+    note = root_amp * envelope(note)  # apply amplitude and envelope
+    stream.write(note.tobytes())
+
+    # "for remaining beats
+    for i in range(1, len(notes)):
+        # "play frequency k for beat interval (sine wave, volume, ramp)
+        note = np.asarray([sine_wave(notes[i], t) for t in np.linspace(0, (1/bps), spb)], dtype=np.float32)
+        note = amp * envelope(note)  # apply amplitude and envelope
+        stream.write(note.tobytes())
 
 
-
-
-
-
-
-
-
-
-
-
-# class for holding the specifications for the audio file
-class Spec:
-    def __init__(self,
-                 channels=1,
-                 bits=16,
-                 amplitude=0.167,
-                 duration=1.0,
-                 sample_rate=48000,
-                 chord="Am",
-                 temperament="just",
-                 ):
-        self.channels = channels
-        self.bits = bits
-        self.amplitude = amplitude
-        self.duration = duration
-        self.sample_rate = sample_rate
-        self.chord = chord
-        self.temperament = temperament
-        self.notes, self.freqs = self.process_c_t()
-
-    # get chord and temperament from user input
-    def input(self):
-        c_t = input("Please Enter a chord and temperament (ex1. 'A equal', ex2. 'Ebm just'): ")
-        self.chord, self.temperament = c_t.split()
-        self.notes, self.freqs = self.process_c_t()
-
-    # get chord and temperament from argument
-    def set(self, c_t):
-        self.chord, self.temperament = c_t.split()
-        self.notes, self.freqs = self.process_c_t()
-
-    # process chord and temperament
-    def process_c_t(self):
-
-        # name_to_key and just_ratios from:
-        # https://github.com/pdx-cs-sound/hw-chord/blob/master/tables.py
-        # Conversion table: Note names to MIDI key numbers.
-        name_to_key = {
-            "C": 72,
-            "Db": 73,
-            "D": 74,
-            "Eb": 75,
-            "E": 76,
-            "F": 77,
-            "Gb": 78,
-            "G": 79,
-            "Ab": 80,
-            "A": 81,
-            "Bb": 82,
-            "B": 83,
-        }
-        just_ratios = [
-            1,
-            16/15,
-            9/8,
-            6/5,
-            5/4,
-            4/3,
-            45/32,
-            3/2,
-            8/5,
-            5/3,
-            9/5,
-            15/8,
-        ]
-
-        # https://gist.github.com/YuxiUx/ef84328d95b10d0fcbf537de77b936cd
-        def midi_to_frequency(note):
-            a = 440
-            return (a / 32) * (2 ** ((note - 9) / 12))
-
-        notes = []
-        freqs = []
-
-        # get frequency of first note
-        if len(self.chord) == 1 or (len(self.chord) == 2 and self.chord[1] == "m"):
-            notes.append(name_to_key[self.chord[0]])
-            freqs.append(midi_to_frequency(notes[0]))
-        else:
-            notes.append(name_to_key[self.chord[0] + self.chord[1]])
-            freqs.append(midi_to_frequency(notes[0]))
-
-        # get frequency of second note
-        if (len(self.chord) == 2 and self.chord[1] == "m") or \
-                (len(self.chord) == 3 and self.chord[2] == "m"):
-            if self.temperament == "equal":
-                notes.append(notes[0] + 3)
-                freqs.append(midi_to_frequency(notes[1]))
-            else:
-                freqs.append(freqs[0] * just_ratios[3])
-        else:
-            if self.temperament == "equal":
-                notes.append(notes[0] + 4)
-                freqs.append(midi_to_frequency(notes[1]))
-            else:
-                freqs.append(freqs[0] * just_ratios[4])
-
-        # get frequency of third note
-        if self.temperament == "equal":
-            notes.append(notes[0] + 7)
-            freqs.append(midi_to_frequency(notes[2]))
-        else:
-            freqs.append(freqs[0] * just_ratios[7])
-
-        return notes, freqs
-
-
-# class which creates samples based on Spec
-class ChordSampler(Spec):
-    # def __init__(self, channels=None, bits=None, amplitude=None, duration=None,
-    #             sample_rate=None, chord=None, temperament=None):
-    #    super().__init__(channels, bits, amplitude, duration, sample_rate, chord, temperament)
-    def __init__(self):
-        super().__init__()
-        # set max value for current number of bits
-        self.max = 2**(self.bits - 1) - 1
-        self.min = -self.max - 1
-        # generate the samples
-        self.samples = None
-        self.generate_samples()
-
-    def generate_samples(self):
-        s1 = ((self.amplitude * self.max) *
-              np.sin((2*np.pi) *
-              (np.arange(self.sample_rate*self.duration)) *
-              (self.freqs[0]/self.sample_rate)))
-        s2 = ((self.amplitude * self.max) *
-              np.sin((2*np.pi) *
-              (np.arange(self.sample_rate*self.duration)) *
-              (self.freqs[1]/self.sample_rate)))
-        s3 = ((self.amplitude * self.max) *
-              np.sin((2*np.pi) *
-              (np.arange(self.sample_rate*self.duration)) *
-              (self.freqs[2]/self.sample_rate)))
-
-        self.samples = s1 + s2 + s3
-
-    # play the samples with pyaudio
-    def play(self):
-        # this should only be used when bytes are set to 16
-        # don't convert the original samples, they will be reused
-        samples = self.samples.astype(np.int16)  # np.Float32, etc
-
-        p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paInt16,  # paFloat32, etc
-                        channels=1,
-                        rate=self.sample_rate,
-                        output=True)
-        # https://stackoverflow.com/questions/8299303 yahweh - tobytes()
-        stream.write(1 * samples.tobytes())
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-    # write the samples to disk
-    def write(self):
-        file = self.chord + "-" + self.temperament + ".wav"
-        samples = self.samples.astype(np.int16)
-        wf.write(file, self.sample_rate, samples)
-
-
-def main():
-    print("Homework 3")
-
-    s = ChordSampler()
-
-    s.input()
-    s.generate_samples()
-    s.write()
-
-    # play a song with some nice chords :)
-    # for i in range(5):
-    #     s.set("C", "equal")
-    #     s.generate_samples()
-    #     s.play()
-    #
-    #     s.set("F", "equal")
-    #     s.generate_samples()
-    #     s.play()
-    #
-    #     s.set("Am", "equal")
-    #     s.generate_samples()
-    #     s.play()
-    #
-    #     s.set("G", "equal")
-    #     s.generate_samples()
-    #     s.play()
-
-
-if __name__ == '__main__':
-    main()
+# tear down pyaudio stream (unreachable)
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
